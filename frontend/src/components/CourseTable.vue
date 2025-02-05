513ba3b730f638994e32b18eb224a3152d3e4394
luca28-04 <luca3@hotmail.co.uk>
2025-02-05 15:25:46 +0000

    mock table implemented with styling

--- frontend/src/components/CourseTable.vue
+++ frontend/src/components/CourseTable.vue
@@ -1,13 +1,51 @@
 <script>
+export default {
+    name: 'CourseTable',
+    props: {
+        courses: {
+            type: Array,
+            required: true,
+        },
+    },
+};
 
 </script>
 
-
 <template>
-
+    <table class="course-table">
+    <thead>
+      <tr>
+        <th>Rank</th>
+        <th>Course</th>
+        <th>University</th>
+        <th>URL</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="course in courses" :key="course.id">
+        <td>{{ course.rank }}</td>
+        <td>{{ course.course }}</td>
+        <td>{{ course.university }}</td>
+        <td><a :href="course.url" target="_blank">{{ course.url }}</a></td>
+      </tr>
+    </tbody>
+  </table>
 </template>
 
 
-<style>
+<style scoped>
+.course-table {
+  width: 80%;
+  border-collapse: collapse;
+  margin: 20rem auto 0;
+}
 
+th, td {
+  padding: 0.5rem;
+  border: 1px solid #ccc;
+}
+
+th {
+  background-color: #f0f0f0;
+}
 </style>
